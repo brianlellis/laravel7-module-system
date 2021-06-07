2567ec734bb43e13b13b6db953df8f5368e632a6
Brian Ellis <brian@briansresume.com>
2021-06-07 04:24:52 -0400

    stabilize

--- Controllers/Tours.php
+++ Controllers/Tours.php
@@ -6,11 +6,11 @@ class Tours
 {
   public static function first_visit()
   { 
-    $first_visit = \DB::table('user_page_visits')
-                    ->where('user_id',\Auth::user()->id)->where('page',request()->path())->first();
+    $first_visit = \m_UserPageVisits::where('user_id',\Auth::user()->id)
+                      ->where('page',request()->path())->first();
 
     if(!$first_visit) {
-      \DB::table('user_page_visits')->insert([
+      \m_UserPageVisits::insert([
         'user_id' => \Auth::user()->id,
         'page'    => request()->path()
       ]);
